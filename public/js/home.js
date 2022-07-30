a697c8142d5321a764e0100106da5ae709713bba
madrigueira <mndias29@gmail.com>
2022-07-29 23:09:10 -0300

    Upando novo projeto (aquela carinha)

--- public/js/home.js
+++ public/js/home.js
@@ -5,3 +5,9 @@ $(function(){
 function togglePopup(){
     document.getElementById("popup").classList.toggle("active")
 }
+
+function toggleMenu(){
+    document.getElementById("menu").classList.toggle("active")
+    document.getElementById("btn-menu").classList.toggle("active")
+    document.getElementById("btn-pesquisa").classList.toggle("active")
+}
